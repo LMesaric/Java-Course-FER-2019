7b81183e2a076d40231ccee8ed94b7ccd80251a7
LMesaric <luka.mesaric@fer.hr>
2019-04-06 15:56:49 +0200

    Add util

---
+++ hw05-0036505985/src/main/java/hr/fer/zemris/util/Util.java
@@ -0,0 +1,27 @@
+package hr.fer.zemris.util;
+
+import java.util.Objects;
+
+/**
+ * Utility class for easily generating customised exceptions.
+ * 
+ * @author Luka Mesaric
+ */
+public final class Util {
+
+	/**
+	 * Checks that the specified object reference is not {@code null} and throws a
+	 * customised {@link NullPointerException} if it is.
+	 * 
+	 * @param object the object reference to check
+	 * @param name   name to use in exception message
+	 * @param        <T> the type of the reference
+	 * @return <code>object</code> if not <code>null</code>
+	 * 
+	 * @throws NullPointerException if <code>object</code> is <code>null</code>
+	 */
+	public static <T> T validateNotNull(T object, String name) {
+		return Objects.requireNonNull(object, "Argument '" + name + "' must not be null.");
+	}
+
+}
