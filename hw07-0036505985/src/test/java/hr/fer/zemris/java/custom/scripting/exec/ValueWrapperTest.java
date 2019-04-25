efd0836854279941ee0941ddb1b409085db81021
LMesaric <luka.mesaric@fer.hr>
2019-04-25 19:47:05 +0200

    Remove forgotten method

--- hw07-0036505985/src/test/java/hr/fer/zemris/java/custom/scripting/exec/ValueWrapperTest.java
+++ hw07-0036505985/src/test/java/hr/fer/zemris/java/custom/scripting/exec/ValueWrapperTest.java
@@ -64,24 +64,6 @@ class ValueWrapperTest {
 	}
 
 	@Test
-	void test() {
-		// TODO
-		ValueWrapper v3 = new ValueWrapper("1.2E1");
-		ValueWrapper v4 = new ValueWrapper(Integer.valueOf(1));
-		v3.add(v4.getValue());
-
-		assertEquals(Double.valueOf(13), v3.getValue());
-		assertEquals(Integer.valueOf(1), v4.getValue());
-
-		ValueWrapper v5 = new ValueWrapper("12");
-		ValueWrapper v6 = new ValueWrapper(Integer.valueOf(1));
-		v5.add(v6.getValue());
-
-		assertEquals(Integer.valueOf(13), v5.getValue());
-		assertEquals(Integer.valueOf(1), v6.getValue());
-	}
-
-	@Test
 	void testIntegerAndInteger() {
 		ValueWrapper v0 = new ValueWrapper(Integer.valueOf(-2));
 
@@ -213,11 +195,17 @@ class ValueWrapperTest {
 		v4.divide(v0.getValue());
 		assertEquals(Double.valueOf(-1.75), v4.getValue());
 
+		ValueWrapper v5 = new ValueWrapper("12");
+		ValueWrapper v6 = new ValueWrapper(Integer.valueOf(1));
+		v5.add(v6.getValue());
+		assertEquals(Integer.valueOf(13), v5.getValue());
+		assertEquals(Integer.valueOf(1), v6.getValue());
+
 		assertEquals("-2", v0.getValue());
 
-		ValueWrapper v5 = new ValueWrapper(Double.valueOf(3));
-		assertTrue(v5.numCompare(v0.getValue()) > 0);
-		assertTrue(v0.numCompare(v5.getValue()) < 0);
+		ValueWrapper v7 = new ValueWrapper(Double.valueOf(3));
+		assertTrue(v7.numCompare(v0.getValue()) > 0);
+		assertTrue(v0.numCompare(v7.getValue()) < 0);
 		assertTrue(v0.numCompare(v0.getValue()) == 0);
 	}
 
@@ -241,11 +229,17 @@ class ValueWrapperTest {
 		v4.divide(v0.getValue());
 		assertEquals(Double.valueOf(-1.75), v4.getValue());
 
+		ValueWrapper v5 = new ValueWrapper("1.2E1");
+		ValueWrapper v6 = new ValueWrapper(Integer.valueOf(1));
+		v5.add(v6.getValue());
+		assertEquals(Double.valueOf(13), v5.getValue());
+		assertEquals(Integer.valueOf(1), v6.getValue());
+
 		assertEquals("-2.0", v0.getValue());
 
-		ValueWrapper v5 = new ValueWrapper(Double.valueOf(3));
-		assertTrue(v5.numCompare(v0.getValue()) > 0);
-		assertTrue(v0.numCompare(v5.getValue()) < 0);
+		ValueWrapper v7 = new ValueWrapper(Double.valueOf(3));
+		assertTrue(v7.numCompare(v0.getValue()) > 0);
+		assertTrue(v0.numCompare(v7.getValue()) < 0);
 		assertTrue(v0.numCompare(v0.getValue()) == 0);
 	}
 
